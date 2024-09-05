c421ef9c2a2ad0fe232305097be453e5802b9610
Maxwell Rumahorbo <maxrumbo06@gmail.com>
2024-09-04 21:31:25 -0700

    SUCCESSED

--- T01.java
+++ T01.java
@@ -1,3 +1,20 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24037 - Maxwell Rumahorbo
+// 12S24030 - Swasti Silaban
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Type Name;
+
+        // Name = input.nextLine();
+        // System.out.println(Expression);
+    }
+}
+
+
+
 
